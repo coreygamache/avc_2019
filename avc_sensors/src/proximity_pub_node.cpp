4d98849ce8acc66a3e5858db982af30f28a7bc74
coreygamache <coreygamache@hotmail.com>
2018-05-31 15:36:47 -0500

    test for piro

--- avc_sensors/src/proximity_pub_node.cpp
+++ avc_sensors/src/proximity_pub_node.cpp
@@ -15,19 +15,24 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "proximity_pub_node");
   ros::NodeHandle node_private("~");
 
-  //get pin parameters or use default values if not found
-  int echo_pin, trigger_pin;
-  if (!node_private.getParam("proximity_sensor/echo_pin"), echo_pin)
+  //get echo pin from parameters
+  int echo_pin;
+  if (!node_private.getParam("proximity_sensor/echo_pin", echo_pin))
   {
-    echo_pin = 4;
-  }
-  if (!node_private.getParam ("proximity_sensor/trigger_pin"), trigger_pin)
-  {
-    trigger_pin = 5;
+    ROS_ERROR("proximity sensor echo pin not defined in config file: avc_sensors/config/sensors.yaml");
+    ROS_BREAK();
   }
 
-  //create Sensor type object with echo set to GPIO pin 23 and trigger to 24
-  ProximitySensor sensor(4, 5);
+  //get trigger pin from parameters
+  int trigger_pin;
+  if (!node_private.getParam ("proximity_sensor/trigger_pin", trigger_pin))
+  {
+    ROS_ERROR("proximity sensor trigger pin not defined in config file: avc_sensors/config/sensors.yaml");
+    ROS_BREAK();
+  }
+
+  //create Sensor type object using defined echo and trigger pin parameters
+  ProximitySensor sensor(echo_pin, trigger_pin);
 
   //create sensor_msgs/Range type message to publish proximity sensor data
   sensor_msgs::Range proximity_msg;
@@ -39,16 +44,40 @@ int main(int argc, char **argv)
   proximity_msg.header.frame_id = "proximity_sensor_link";
 
   //set radiation type of sensor (0 indicates ultrasound, 1 infrared)
-  proximity_msg.radiation_type = 0;
+  int radiation_type;
+  if (!node_private.getParam("proximity_sensor/radiation_type", radiation_type))
+  {
+    ROS_ERROR("proximity sensor radiation type not defined in config file: avc_sensors/config/sensors.yaml");
+    ROS_BREAK();
+  }
+  proximity_msg.radiation_type = radiation_type;
 
   //set field of view of sensor in radians
-  proximity_msg.field_of_view = 0.3665;
+  float field_of_view;
+  if (!node_private.getParam("proximity_sensor/field_of_view", field_of_view))
+  {
+    ROS_ERROR("proximity sensor field of view not defined in config file: avc_sensors/config/sensors.yaml");
+    ROS_BREAK();
+  }
+  proximity_msg.field_of_view = field_of_view;
 
   //set minimum range of sensor in meters
-  proximity_msg.min_range = 0.05;
+  float min_range;
+  if (!node_private.getParam("proximity_sensor/min_range", min_range))
+  {
+    ROS_ERROR("proximity sensor minimum range not defined in config file: avc_sensors/config/sensors.yaml");
+    ROS_BREAK();
+  }
+  proximity_msg.min_range = min_range;
 
   //set maximum range of sensor in meters
-  proximity_msg.max_range = 2.0;
+  float max_range;
+  if (!node_private.getParam("proximity_sensor/max_range", max_range))
+  {
+    ROS_ERROR("proximity sensor maximum range not defined in config file: avc_sensors/config/sensors.yaml");
+    ROS_BREAK();
+  }
+  proximity_msg.max_range = max_range;
 
   //----------------------------------------------------------
 
