b8c44bc8bd8487b2ab98df2b679719e4d5f6ccbd
coreygamache <coreygamache@hotmail.com>
2019-02-02 18:01:57 -0600

    prototype node for testing

--- avc_control/src/manual_control_node.cpp
+++ avc_control/src/manual_control_node.cpp
@@ -2,36 +2,43 @@
 //this node handles manual control of the robot when autonomous control is disabled
 #include <errno.h>
 #include <ros/ros.h>
+#include <avc_msgs/ChangeControlMode.h>
 #include <avc_msgs/Control.h>
+#include <avc_msgs/ESC.h>
+#include <avc_msgs/SteeringServo.h>
 #include <sensor_msgs/Joy.h>
 #include <signal.h>
-#include <wiringPi.h>
 
 //global variables
 bool autonomous_control = false;
-bool mode_change_requested = false;
 
 //global controller variables
 std::vector<float> controller_axes(8, 0);
 std::vector<int> controller_buttons(13, 0);
 
-//pin variables
-//must be global so that they can be accessed by callback function
-int mode_LED_blue_pin;
-int mode_LED_green_pin;
-int mode_LED_red_pin;
 
 //callback function called to process SIGINT command
 void sigintHandler(int sig)
 {
 
-  //set all pins LOW
-  digitalWrite(mode_LED_blue_pin, LOW);
-  digitalWrite(mode_LED_green_pin, LOW);
-  digitalWrite(mode_LED_red_pin, LOW);
-
   //call the default shutdown function
   ros::shutdown();
+
+}
+
+//callback function called to process messages on control topic
+void controlCallback(const avc_msgs::Control::ConstPtr& msg)
+{
+
+  //verify that local mode matches global mode
+  if (autonomous_control != msg->autonomous_control)
+  {
+
+    //modes do not match; send notification and shut down node
+    ROS_INFO("[manual_control_mode] local control mode does not match global control mode; killing program");
+    ROS_BREAK();
+
+  }
 
 }
 
@@ -40,20 +47,38 @@ void controllerCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
 
   //set local values to match message values
+  controller_axes = msg->axes;
   controller_buttons = msg->buttons;
 
-  //if mode change controller button is pressed then set change mode request true
-  if (controller_buttons[10] == 1)
+}
+
+//callback function called to process service requests on the disable mapping mode topic
+bool disableManualControlCallback(avc_msgs::ChangeControlMode::Request& req, avc_msgs::ChangeControlMode::Response& res)
+{
+
+  //if node isn't currently busy then ready to change modes, otherwise not ready to change
+  res.ready_to_change = true;
+
+  //output ROS INFO message to inform of mode change request and reply status
+  if (req.mode_change_requested && res.ready_to_change)
   {
 
-    //set mode change requested to true to indicate request to change modes
-    mode_change_requested = true;
+    //change modes
+    autonomous_control = !autonomous_control;
 
-    //reset controller button if pressed to prevent mode from toggling twice on one button press
-    if (controller_buttons[10] == 1)
-      controller_buttons[10] = 0;
+    //output notification
+    ROS_INFO("[manual_control_mode] mode change requested; changing control modes");
 
   }
+  else if (!req.mode_change_requested && res.ready_to_change)
+    ROS_INFO("[manual_control_mode] ready to change modes status requested; indicating ready to change");
+  else if (req.mode_change_requested && !res.ready_to_change)
+    ROS_INFO("[manual_control_mode] mode change requested; indicating node is busy");
+  else
+    ROS_INFO("[manual_control_mode] ready to change modes status requested; indicating node is busy");
+
+  //return true to indicate service processing is complete
+  return true;
 
 }
 
@@ -61,84 +86,54 @@ int main(int argc, char **argv)
 {
 
   //send notification that node is launching
-  ROS_INFO("[NODE LAUNCH]: starting control_node");
+  ROS_INFO("[NODE LAUNCH]: starting manual_control_mode");
 
   //initialize node and create node handler
-  ros::init(argc, argv, "control_node");
+  ros::init(argc, argv, "manual_control_mode");
   ros::NodeHandle node_private("~");
   ros::NodeHandle node_public;
 
   //override the default SIGINT handler
   signal(SIGINT, sigintHandler);
 
-  //retrieve mode LED pin (blue) from parameter server
-  if (!node_private.getParam("/led/mode_blue_pin", mode_LED_blue_pin))
+  //retrieve steering servo max rotation angle from parameter server
+  float servo_max_angle;
+  if (!node_private.getParam("/steering_servo/max_rotation_angle", servo_max_angle))
   {
-    ROS_ERROR("[map_waypoints_node] mode LED pin (blue) not defined in config file: avc_bringup/config/global.yaml");
-    ROS_BREAK();
-  }
-
-  //retrieve mode LED pin (green) from parameter server
-  if (!node_private.getParam("/led/mode_green_pin", mode_LED_green_pin))
-  {
-    ROS_ERROR("[map_waypoints_node] mode LED pin (green) not defined in config file: avc_bringup/config/global.yaml");
-    ROS_BREAK();
-  }
-
-  //retrieve mode LED pin (red) from parameter server
-  if (!node_private.getParam("/led/mode_red_pin", mode_LED_red_pin))
-  {
-    ROS_ERROR("[map_waypoints_node] mode LED pin (red) not defined in config file: avc_bringup/config/global.yaml");
+    ROS_ERROR("[manual_control_node] steering servo max rotation angle not defined in config file: avc_bringup/config/global.yaml");
     ROS_BREAK();
   }
 
   //retrieve refresh rate of node in hertz from parameter server
   float refresh_rate;
-  if (!node_private.getParam("/control/control_node/refresh_rate", refresh_rate))
+  if (!node_private.getParam("/control/manual_control_node/refresh_rate", refresh_rate))
   {
-    ROS_ERROR("[control_node] control node refresh rate not defined in config file: avc_control/config/control.yaml");
+    ROS_ERROR("[manual_control_node] manual control node refresh rate not defined in config file: avc_control/config/control.yaml");
     ROS_BREAK();
   }
 
-  //create control message object and set default parameters
-  avc_msgs::Control control_msg;
-  control_msg.header.frame_id = "0";
+  //create ESC message object and set default parameters
+  avc_msgs::ESC esc_msg;
+  esc_msg.header.frame_id = "0";
 
-  //create disable mapping service object and set default parameters
-  avc_msgs::DisableMapping disable_mapping_srv;
-  disable_mapping_srv.request.mode_change_requested = true;
+  //create steering servo message object and set default parameters
+  avc_msgs::SteeringServo steering_servo_msg;
+  steering_servo_msg.header.frame_id = "0";
 
-  //create disable navigation object and set default parameters
-  avc_msgs::DisableNavigation disable_navigation_srv;
-  disable_navigation_srv.request.mode_change_requested = true;
+  //create publisher to publish ESC message status with buffer size 10, and latch set to false
+  ros::Publisher esc_pub = node_public.advertise<avc_msgs::ESC>("esc", 10, false);
 
-  //create publisher to publish control message status with buffer size 10, and latch set to true
-  ros::Publisher control_pub = node_public.advertise<avc_msgs::Control>("control", 10, true);
+  //create publisher to publish steering servo message status with buffer size 10, and latch set to false
+  ros::Publisher steering_servo_pub = node_public.advertise<avc_msgs::SteeringServo>("steering_servo", 10, false);
 
-  //create service client to send service requests on the disable mapping topic
-  ros::ServiceClient disable_mapping_clt = node_public.serviceClient<avc_msgs::DisableMapping>("disable_mapping");
+  //create service to process service requests on the disable manual control topic
+  ros::ServiceServer disable_manual_control_srv = node_public.advertiseService("disable_manual_control", disableManualControlCallback);
 
-  //create service client to send service requests on the disable navigation topic
-  ros::ServiceClient disable_navigation_clt = node_public.serviceClient<avc_msgs::DisableNavigation>("disable_navigation");
+  //create subscriber to subscribe to control messages topic with queue size set to 1000
+  ros::Subscriber control_sub = node_public.subscribe("control", 1000, controlCallback);
 
   //create subscriber to subscribe to joy messages topic with queue size set to 1000
   ros::Subscriber controller_sub = node_public.subscribe("joy", 1000, controllerCallback);
-
-  //run wiringPi GPIO setup function and set pin modes
-  wiringPiSetup();
-  pinMode(mode_LED_blue_pin, OUTPUT);
-  pinMode(mode_LED_green_pin, OUTPUT);
-  pinMode(mode_LED_red_pin, OUTPUT);
-
-  //create variable for status of autonomous control
-  bool autonomous_control = false;
-
-  //initialize control message to disable autonomous control globally
-  control_msg.header.stamp = ros::Time::now();
-  control_msg.autonomous_control = false;
-
-  //publish initial control message to disable autonomous control globally
-  control_pub.publish(control_msg);
 
   //set loop rate in Hz
   ros::Rate loop_rate(refresh_rate);
@@ -146,69 +141,31 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
 
-    if (mode_change_requested)
+    if (!autonomous_control)
     {
 
-      //reset mode change requested to prevent mode from toggling twice on one button press
-      mode_change_requested = false;
+      //ESC control handling
+      //set time of ESC message
+      esc_msg.header.stamp = ros::Time::now();
 
+      //translate controller axis value to percentage and set to esc msg value
+      esc_msg.throttle_percent = ((controller_axes[5] - 1) / -2) * 100;
 
-      //wait for verification from mapping program that it's safe to change modes
-      do
-      {
+      //publish drive motors message
+      esc_pub.publish(esc_msg);
 
-        //call service and verify success
-        if (!disable_mapping_clt.call(disable_mapping_srv))
-        {
-          ROS_INFO("[control_node] failed to call disable mapping service");
-          ROS_BREAK();
-        }
+      //steering servo control handling
+      //set time of steering servo message
+      steering_servo_msg.header.stamp = ros::Time::now();
 
-      }
-      while (!disable_mapping_srv.response.ready_to_change);
+      //translate controller axis value to percentage and set to esc msg value
+      steering_servo_msg.steering_angle = controller_axes[0] * servo_max_angle;
 
-      //wait for verification from navigation program that it's safe to change modes
-      do
-      {
-
-        if (!disable_navigation_clt.call(disable_navigation_srv))
-        {
-          ROS_INFO("[control_node] failed to call disable mapping service");
-          ROS_BREAK();
-        }
-
-      }
-      while (!disable_navigation_srv.response.ready_to_change);
-
-      //if mapping and navigation programs are ready to change control modes, then switch control modes and publish message
-      if (disable_mapping_srv.response.ready_to_change && disable_navigation_srv.response.ready_to_change)
-      {
-
-        //switch control modes
-        autonomous_control = !autonomous_control;
-
-        //set time and parameters of control message
-        control_msg.header.stamp = ros::Time::now();
-        control_msg.autonomous_control = autonomous_control;
-
-        //publish control message
-        control_pub.publish(control_msg);
-
-        //output result
-        if (autonomous_control)
-          ROS_INFO("[control_node] control mode changed: entering autonomous control mode");
-        else
-          ROS_INFO("[control_node] control mode changed: entering mapping control mode");
-
-        //reset message parameters
-        disable_mapping_srv.response.ready_to_change = false;
-        disable_navigation_srv.response.ready_to_change = false;
-
-      }
+      //publish drive motors message
+      steering_servo_pub.publish(steering_servo_msg);
 
     }
 
   }
-
   return 0;
 }
